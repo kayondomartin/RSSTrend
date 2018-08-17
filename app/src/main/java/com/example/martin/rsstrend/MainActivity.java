76b54e503b901ff2bccef8fca875ab5d24a82adb
kayondomartin <kayondomartin45@gmail.com>
2018-08-17 19:28:47 +0900

    added everything to commit

--- app/src/main/java/com/example/martin/rsstrend/MainActivity.java
+++ app/src/main/java/com/example/martin/rsstrend/MainActivity.java
@@ -4,9 +4,11 @@ package com.example.martin.rsstrend;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.estimote.sdk.Beacon;
 import com.estimote.sdk.BeaconManager;
@@ -14,7 +16,10 @@ import com.estimote.sdk.Region;
 import com.estimote.sdk.SystemRequirementsChecker;
 import com.estimote.sdk.eddystone.Eddystone;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 
@@ -25,11 +30,14 @@ public class MainActivity extends AppCompatActivity {
     BeaconManager.RangingListener rangingListener;
     private Region region;
 
+    private static boolean isRanging = false;
+
     private EditText fileNameEditText;
-    private Button startButton;
-    private Button stopButton;
+    private static Button startButton;
+    private static Button stopButton;
 
     private FileOutputStream fileOutputStream;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,14 +51,60 @@ public class MainActivity extends AppCompatActivity {
             public void onBeaconsDiscovered(Region region, List<Beacon> list) {
                 if(!list.isEmpty()){
                     Beacon gotBeacon = list.get(0);
+                    String fileName = fileNameEditText.getText().toString().trim() + ".txt";
+                    String address = gotBeacon.getMacAddress().toString();
+                    int rssi = gotBeacon.getRssi();
+
+
+                    try {
+                        fileOutputStream = new FileOutputStream(fileName,true);
+                        FileWriter fileWriter;
+
+                        try{
+                            fileWriter = new FileWriter(fileOutputStream.getFD());
+                            fileWriter.write(address+"\t"+rssi+"\n");
+                            fileWriter.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }finally {
+                            fileOutputStream.getFD().sync();
+                            fileOutputStream.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         };
-
-
-
-
         beaconManager.setRangingListener(rangingListener);
+
+        startButton = (Button) findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(fileNameEditText.getText().toString().trim() == null){
+                    Toast.makeText(MainActivity.this,"Enter file name",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+                    @Override
+                    public void onServiceReady() {
+                        beaconManager.startRanging(region);
+                        isRanging = true;
+                    }
+                });
+            }
+        });
+
+        stopButton = (Button) findViewById(R.id.stop_button);
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                beaconManager.stopRanging(region);
+                isRanging = false;
+            }
+        });
 
 
         region = new Region("ranged region",UUID.fromString(BeaconContract.Beacon3.UUID), BeaconContract.Beacon3.major, BeaconContract.Beacon3.minor);
@@ -58,18 +112,22 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    static {
+        if(isRanging){
+            startButton.setClickable(false);
+            stopButton.setClickable(true);
+        }else{
+            startButton.setClickable(true);
+            stopButton.setClickable(false);
+        }
+    }
+
+
 
     @Override
     protected void onResume() {
         super.onResume();
         SystemRequirementsChecker.checkWithDefaultDialogs(this);
-
-        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
-            @Override
-            public void onServiceReady() {
-                beaconManager.startRanging(region);
-            }
-        });
     }
 
     @Override
