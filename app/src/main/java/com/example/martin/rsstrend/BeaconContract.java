445493590537667b146746659d64d4c4dbe027cf
kayondomartin <kayondomartin45@gmail.com>
2018-08-17 17:16:36 +0900

    Update BeaconContract.java

--- app/src/main/java/com/example/martin/rsstrend/BeaconContract.java
+++ app/src/main/java/com/example/martin/rsstrend/BeaconContract.java
@@ -7,6 +7,7 @@ public class BeaconContract {
         public static final String UUID = "BCFE85CB-0666-8DEF-CD5D-719E6EA924F6";
         public static final int major = 41216;
         public static final int minor = 15619;
+        public static final int check = 0;
     }
 
 }
