bb83cacb927a56220cf2cee06b604b4412f2cdc3
kayondomartin <kayondomartin45@gmail.com>
2018-08-20 12:38:43 +0900

    added everything to commit

--- app/src/main/java/com/example/martin/rsstrend/MainActivity.java
+++ app/src/main/java/com/example/martin/rsstrend/MainActivity.java
@@ -4,24 +4,18 @@ package com.example.martin.rsstrend;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.estimote.sdk.Beacon;
 import com.estimote.sdk.BeaconManager;
 import com.estimote.sdk.Region;
 import com.estimote.sdk.SystemRequirementsChecker;
-import com.estimote.sdk.eddystone.Eddystone;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
@@ -33,11 +27,10 @@ public class MainActivity extends AppCompatActivity {
     BeaconManager.RangingListener rangingListener;
     private Region region;
 
-    private static boolean isRanging = false;
-
     private EditText fileNameEditText;
-    private static Button startButton;
-    private static Button stopButton;
+    private Button startButton;
+
+    private boolean isRanging = false;
 
     private FileOutputStream fileOutputStream;
 
@@ -79,12 +72,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onKey(View view, int i, KeyEvent keyEvent) {
                 if(keyEvent.getAction() == KeyEvent.ACTION_DOWN && i == KeyEvent.KEYCODE_ENTER){
-                    if(fileNameEditText.getText().toString().trim() == null){
+                    if(fileNameEditText.getText().toString().trim() == null || fileNameEditText.getText().toString().isEmpty()){
                         Toast.makeText(MainActivity.this,"Enter file name",Toast.LENGTH_SHORT).show();
                         return false;
+                    }else{
+                        startRanging();
                     }
-
-                    startRanging();
                 }
                 return true;
             }
@@ -95,25 +88,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                if(fileNameEditText.getText().toString().trim() == null){
+                if(fileNameEditText.getText().toString().trim() == null || fileNameEditText.getText().toString().isEmpty()){
                     Toast.makeText(MainActivity.this,"Enter file name",Toast.LENGTH_SHORT).show();
                     return;
+                }else{
+                    startRanging();
                 }
 
-                startRanging();
-
             }
         });
-
-        stopButton = (Button) findViewById(R.id.stop_button);
-        stopButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                beaconManager.stopRanging(region);
-                isRanging = false;
-            }
-        });
-
 
         region = new Region("ranged region",UUID.fromString(BeaconContract.Beacon3.UUID), BeaconContract.Beacon3.major, BeaconContract.Beacon3.minor);
 
@@ -125,20 +108,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onServiceReady() {
                 beaconManager.startRanging(region);
+                fileNameEditText.setEnabled(false);
+                startButton.setText("STOP");
                 isRanging = true;
             }
         });
     }
 
-    static {
-        if(isRanging && stopButton != null && startButton != null){
-            startButton.setClickable(false);
-            stopButton.setClickable(true);
-        }else if(stopButton != null && startButton != null){
-            startButton.setClickable(true);
-            stopButton.setClickable(false);
+
+
+    private void stopRanging(){
+        if(isRanging){
+            beaconManager.stopRanging(region);
+            fileNameEditText.setEnabled(true);
+            startButton.setText("START");
+            isRanging = false;
         }
     }
+
+
 
 
 
@@ -146,11 +134,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         SystemRequirementsChecker.checkWithDefaultDialogs(this);
+        startRanging();
     }
 
     @Override
     protected void onPause() {
-        beaconManager.stopRanging(region);
         super.onPause();
+        beaconManager.stopRanging(region);
     }
 }
